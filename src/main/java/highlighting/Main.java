ceb4c71a9d9ed5f33d890f6918664392b25ce1f1
Carsten Gips <cagix@hsbi.de>
2025-04-18 15:18:12 +0200

    make spotless happy (modified settings)

--- src/main/java/highlighting/Main.java
+++ src/main/java/highlighting/Main.java
@@ -1,6 +1,5 @@
 package highlighting;
 
-import java.awt.*;
 import java.util.List;
 
 /** Configure the lexer and start the demo. */
@@ -13,38 +12,38 @@ public class Main {
     public static void main(String... args) {
         String defaultText =
                 """
-        package controller;
+package controller;
 
-        import com.badlogic.gdx.Game;
-        import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-        /* ApplicationListener that delegates to the MainGameController. Just some setup. */
-        public class LibgdxSetup extends Game {
-            private final MainController mc;
+/* ApplicationListener that delegates to the MainGameController. Just some setup. */
+public class LibgdxSetup extends Game {
+    private final MainController mc;
 
-            /**
-             * The batch is necessary to draw ALL the stuff. Every object that uses draw need to know the
-             * batch.
-             */
-            private SpriteBatch batch;
-            // This batch is used to //draw the HUD /*elements*/ on it.\040
-            private SpriteBatch hudBatch;
+    /**
+     * The batch is necessary to draw ALL the stuff. Every object that uses draw need to know the
+     * batch.
+     */
+    private SpriteBatch batch;
+    // This batch is used to //draw the HUD /*elements*/ on it.\040
+    private SpriteBatch hudBatch;
 
-            /**
-             * "ApplicationListener" that delegates to the "MainGameController". Just some setup.
-             */
-            public LibgdxSetup(MainController mc) {
-                this.mc = mc;
-            }
+    /**
+     * "ApplicationListener" that delegates to the "MainGameController". Just some setup.
+     */
+    public LibgdxSetup(MainController mc) {
+        this.mc = mc;
+    }
 
-            @Over-ride 'someText'
-            public void create() {
-                // new ...
-                char ch = new Character('a');
-                return null;
-            }
-        }
-        """;
+    @Over-ride 'someText'
+    public void create() {
+        // new ...
+        char ch = new Character('a');
+        return null;
+    }
+}
+""";
 
         LexerUI.show(defaultText, Lexer.of(setupTokens()));
     }
